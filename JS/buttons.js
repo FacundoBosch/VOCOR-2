4cc94827d12856131eb032ddfb304c95b5f84abd
FacundoBosch <facundomassin@gmail.com>
2023-01-27 14:05:23 -0300

    didnt work as planned

--- JS/buttons.js
+++ JS/buttons.js
@@ -3,13 +3,13 @@ let linkButton2 = document.getElementById('animation2')
 let linkButton3 = document.getElementById('animation3')
 
 linkButton1.addEventListener('click', () => {
-  location.pathname = './normalPlan.html'
+  location.pathname = '/VOCOR-different-style/normalPlan.html'
 })
 
 linkButton2.addEventListener('click', () => {
-  location.pathname = './404NotFound.html'
+  location.pathname = '/VOCOR-different-style/404NotFound.html'
 })
 
 linkButton3.addEventListener('click', () => {
-  location.pathname = './404NotFound.html'
+  location.pathname = '/VOCOR-different-style/404NotFound.html'
 })
