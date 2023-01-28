bd2c2e19d7a33bfc6d287f57362ea8c9694b7c13
FacundoBosch <facundomassin@gmail.com>
2023-01-28 13:41:09 -0300

    pro plan added

--- JS/animationsMain.js
+++ JS/animationsMain.js
@@ -1,6 +1,7 @@
 const Animation1 = document.getElementById('animation1')
 const Animation2 = document.getElementById('animation2')
 const Animation3 = document.getElementById('animation3')
+const Animation4 = document.getElementById('animation4')
 
 const cargarImagen = (entradas, observador) => {
 
@@ -21,3 +22,8 @@ const observador = new IntersectionObserver(cargarImagen, {
 observador.observe(Animation1)
 observador.observe(Animation2)
 observador.observe(Animation3)
+try {
+  observador.observe(Animation4)
+} catch (error) {
+  console.log(error);
+}
